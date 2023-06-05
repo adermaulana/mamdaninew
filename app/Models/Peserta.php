9f44caa70918e29770c954379706005eaa065309
Ade Rahmat Maulana <adermaulana15@gmail.com>
2023-06-05 10:36:44 +0800

    fix it model

--- app/Models/Peserta.php
+++ app/Models/Peserta.php
@@ -5,16 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-<<<<<<< HEAD
 
 class Peserta extends Authenticatable
 {
     use HasFactory;
-=======
-class Peserta extends Authenticatable
-{
-    use HasFactory;
-
->>>>>>> 0a6ed4774898afc8e2843b30e53313f222c83898
     protected $guarded = ['id'];
 }
